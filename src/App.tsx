8e6acc553e977f76580784a0fc5c35e332266df9
purnimagupta27 <purnimagupta2709@gmail.com>
2026-09-22 17:37:42 +0530

    add Redux store setup with user slice and integrate into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import Browse from './components/Browse'
 import Login from './components/Login'
+import {Provider} from 'react-redux'
+import appStore from './utils/appStore'
 
 const App = () => {
   return (
     <div className="text-2xl">
-      <Routes>
+      <Provider store={appStore}>
+        <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/browse" element={<Browse />} />
       </Routes>
+      </Provider>
     </div>
   )
 }
